8986da1337f050c183af7ca9a2449dab3593bcae
Ashley Salzetti <asalzetti@gmail.com>
2017-02-03 15:21:02 -0800

    Added modularity and better naming schemes

--- cookbookRecipes.py
+++ cookbookRecipes.py
@@ -1,21 +1,42 @@
 from pathlib import Path
 from xml.dom.minidom import parseString
 
+def numbRecipes(txtFilesFolder):
+    count = 0
+    for file in txtFilesFolder.iterdir():
+        if file.is_file():
+            tags = getFileTags(file)
+            count += len(tags.getElementsByTagName('recipe'))
+    return count
 
+def recipeNames(txtFilesFolder):
+    recipes = []
+    for file in txtFilesFolder.iterdir():
+        if file.is_file():
+            tags = getFileTags(file)
+            for recipe in tags.getElementsByTagName('recipe'):
+                try:
+                    titleTag = recipe.getElementsByTagName('purpose')[0]
+                    title = titleTag.firstChild.data
+                    recipes.append(title)
+                except:
+                    pass
+    return recipes
 
-p = Path('C:\\Python36\\cookbook_textencoded')
-numb = 0
-for x in p.iterdir():
-    if x.is_file():
-        file = open(str(x), 'r')
-        data = file.read()
-        file.close()
-        dom = parseString(data)
-        numb += len(dom.getElementsByTagName('recipe'))
-        for item in dom.getElementsByTagName('recipe'):
-            name = item.getElementsByTagName('purpose')[0]
-            try:
-                print(name.firstChild.data)
-            except:
-                pass
-print(numb)
+def printRecipeNames(recipeNames):
+    print("Names of recipes:")
+    for index in range(len(recipeNames)):
+        print(str(index) + ': ' + recipeNames[index])
+
+def getFileTags(file):
+    txtFile = open(str(file), 'r')
+    txt = txtFile.read()
+    txtFile.close()
+    tags = parseString(txt)
+    return tags
+
+if __name__ == '__main__':
+    recipeFilesFolder = Path('C:\\Python36\\cookbook_textencoded')
+    print("Number of recipes:", numbRecipes(recipeFilesFolder))
+    printRecipeNames(recipeNames(recipeFilesFolder))
+    
